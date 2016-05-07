913d3c115847eabab7fa0bc62cc79e8de27bc94e
Hanawa Takuro <takuro@Broccoli.local>
2016-05-07 16:23:02 +0900

    Initial commit

---
+++ DiggTests/DiggTests.swift
@@ -0,0 +1,36 @@
+//
+//  DiggTests.swift
+//  DiggTests
+//
+//  Created by Hanawa Takuro on 2016/05/07.
+//  Copyright © 2016年 Hanawa Takuro. All rights reserved.
+//
+
+import XCTest
+@testable import Digg
+
+class DiggTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
